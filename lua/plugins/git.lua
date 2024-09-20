77d50bb8f5a783d18fd426524e190587085b2493
Adienox <adwaitadk@pm.me>
2024-09-20 16:56:36 +0545

    fix: remove lazygit outer border

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -93,6 +93,9 @@ return {
     dependencies = {
       'nvim-lua/plenary.nvim',
     },
+    config = function()
+      vim.g.lazygit_floating_window_border_chars = { '', '', '', '', '', '', '', '' } -- customize lazygit popup window border characters
+    end,
     keys = {
       {
         '<leader>gg',
