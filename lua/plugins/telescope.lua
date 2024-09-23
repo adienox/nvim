6f0ee66f9feb337d242d705f5019939f7ec0191c
Adienox <adwaitadk@pm.me>
2024-09-23 15:07:32 +0545

    feat: updated telescope config

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -6,22 +6,23 @@ return { -- Fuzzy Finder (files, lsp, etc)
   },
   branch = '0.1.x',
   dependencies = {
-    'nvim-lua/plenary.nvim',
-    { -- If encountering errors, see telescope-fzf-native README for installation instructions
+    {
       'nvim-telescope/telescope-fzf-native.nvim',
       build = 'make',
       cond = function()
         return vim.fn.executable 'make' == 1
       end,
     },
+    'nvim-lua/plenary.nvim',
     'nvim-telescope/telescope-ui-select.nvim',
-
-    -- Useful for getting pretty icons, but requires a Nerd Font.
     'nvim-tree/nvim-web-devicons',
   },
   config = function()
+    local telescope = require 'telescope'
     local actions = require 'telescope.actions'
-    require('telescope').setup {
+    local builtin = require 'telescope.builtin'
+
+    telescope.setup {
       defaults = {
         mappings = {
           i = {
@@ -29,71 +30,24 @@ return { -- Fuzzy Finder (files, lsp, etc)
             ['<C-k>'] = actions.move_selection_previous,
           },
         },
-        file_ignore_patterns = { '.git/', 'node_modules' },
         layout_config = {
           height = 0.90,
           width = 0.90,
-          preview_cutoff = 0,
           horizontal = { preview_width = 0.60 },
           vertical = { width = 0.55, height = 0.9, preview_cutoff = 0 },
           prompt_position = 'top',
         },
-        borderchars = {
-          { '─', '│', '─', '│', '┌', '┐', '┘', '└' },
-          prompt = { '─', '│', ' ', '│', '┌', '┐', '│', '│' },
-          results = { '─', '│', '─', '│', '├', '┤', '┘', '└' },
-          preview = { '─', '│', '─', '│', '┌', '┐', '┘', '└' },
-        },
-        path_display = { 'smart' },
-        prompt_position = 'top',
         prompt_prefix = '  ',
         selection_caret = ' ',
         sorting_strategy = 'ascending',
-        vimgrep_arguments = {
-          'rg',
-          '--color=never',
-          '--no-heading',
-          '--hidden',
-          '--with-filename',
-          '--line-number',
-          '--column',
-          '--smart-case',
-          '--trim', -- add this value
+        borderchars = {
+          { '─', '│', '─', '│', '┌', '┐', '┘', '└' },
+          prompt = { '─', '│', '─', '│', '┌', '┐', '┘', '└' },
+          results = { '─', '│', '─', '│', '┌', '┐', '┘', '└' },
+          preview = { '─', '│', '─', '│', '┌', '┐', '┘', '└' },
         },
       },
-      pickers = {
-        buffers = {
-          prompt_prefix = '󰸩  ',
-        },
-        commands = {
-          prompt_prefix = '  ',
-          layout_config = {
-            height = 0.63,
-            width = 0.78,
-          },
-        },
-        command_history = {
-          prompt_prefix = '  ',
-          layout_config = {
-            height = 0.63,
-            width = 0.58,
-          },
-        },
-        git_files = {
-          prompt_prefix = '󰊢  ',
-          show_untracked = true,
-        },
-        find_files = {
-          prompt_prefix = '  ',
-          find_command = { 'fd', '-H' },
-        },
-        live_grep = {
-          prompt_prefix = '󰱽  ',
-        },
-        grep_string = {
-          prompt_prefix = '󰱽  ',
-        },
-      },
+
       extensions = {
         ['ui-select'] = {
           require('telescope.themes').get_dropdown(),
@@ -102,44 +56,19 @@ return { -- Fuzzy Finder (files, lsp, etc)
     }
 
     -- Enable Telescope extensions if they are installed
-    pcall(require('telescope').load_extension, 'fzf')
-    pcall(require('telescope').load_extension, 'ui-select')
+    pcall(telescope.load_extension, 'fzf')
+    pcall(telescope.load_extension, 'ui-select')
 
     -- See `:help telescope.builtin`
-    local builtin = require 'telescope.builtin'
-    -- vim.keymap.set('n', '<leader>sb', builtin.buffers, { desc = '[S]earch [B]uffers' })
-    vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
-    vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-    vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
-    vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
-    vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-    vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
-    vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
-    vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
-    vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
-    -- vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
-
-    -- Slightly advanced example of overriding default behavior and theme
-    vim.keymap.set('n', '<leader>/', function()
-      -- You can pass additional configuration to Telescope to change the theme, layout, etc.
-      builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-        winblend = 10,
-        previewer = false,
-      })
-    end, { desc = '[/] Fuzzily search in current buffer' })
-
-    -- It's also possible to pass additional configuration options.
-    --  See `:help telescope.builtin.live_grep()` for information about particular keys
-    vim.keymap.set('n', '<leader>s/', function()
-      builtin.live_grep {
-        grep_open_files = true,
-        prompt_title = 'Live Grep in Open Files',
-      }
-    end, { desc = '[S]earch [/] in Open Files' })
-
-    -- Shortcut for searching your Neovim configuration files
-    vim.keymap.set('n', '<leader>sn', function()
-      builtin.find_files { cwd = vim.fn.stdpath 'config' }
-    end, { desc = '[S]earch [N]eovim files' })
+    local set = vim.keymap.set
+    set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
+    set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
+    set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+    set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
+    set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
+    set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
+    set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
+    set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
+    set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
   end,
 }
