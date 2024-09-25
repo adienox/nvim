e727b1308b206b94a28db9cf792b74a82f6b24ae
Adienox <adwaitadk@pm.me>
2024-09-25 20:48:22 +0545

    chore: remove comments

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -57,28 +57,23 @@ return {
 
   {
     'okuuva/auto-save.nvim',
-    cmd = 'ASToggle', -- optional for lazy loading on command
-    event = { 'InsertLeave' }, -- optional for lazy loading on trigger events
+    cmd = 'ASToggle',
+    event = { 'InsertLeave' },
     opts = {
-      enabled = true, -- start auto-save when the plugin is loaded (i.e. when your package manager loads it)
+      enabled = true,
       execution_message = {
         enabled = false,
       },
-      trigger_events = { -- See :h events
-        immediate_save = { 'BufLeave', 'FocusLost' }, -- vim events that trigger an immediate save
-        defer_save = {}, -- vim events that trigger a deferred save (saves after `debounce_delay`)
-        cancel_defered_save = { 'InsertEnter' }, -- vim events that cancel a pending deferred save
+      trigger_events = {
+        immediate_save = { 'BufLeave', 'FocusLost' },
+        defer_save = {},
+        cancel_defered_save = { 'InsertEnter' },
       },
-      -- function that takes the buffer handle and determines whether to save the current buffer or not
-      -- return true: if buffer is ok to be saved
-      -- return false: if it's not ok to be saved
-      -- if set to `nil` then no specific condition is applied
       condition = nil,
-      write_all_buffers = false, -- write all buffers when the current one meets `condition`
-      noautocmd = false, -- do not execute autocmds when saving
-      lockmarks = false, -- lock marks when saving, see `:h lockmarks` for more details
-      debounce_delay = 500, -- delay after which a pending save is executed
-      -- log debug messages to 'auto-save.log' file in neovim cache directory, set to `true` to enable
+      write_all_buffers = false,
+      noautocmd = false,
+      lockmarks = false,
+      debounce_delay = 500,
       debug = false,
     },
   },
