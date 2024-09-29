f9c201ad4758873b806a3c3c600b1b9383e9a986
Adienox <adwaitadk@pm.me>
2024-09-29 17:33:35 +0545

    chore: remove unused bindings

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -28,11 +28,3 @@ set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic m
 set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
 -- set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
 set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
-
--- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
--- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
--- is not what someone will guess without a bit more experience.
---
--- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
--- or just use <C-\><C-n> to exit terminal mode
-set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
