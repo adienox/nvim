5a0b46530c11e39f5e05ddbae5f73018cb94c69a
Adienox <adwaitadk@pm.me>
2024-09-30 23:15:17 +0545

    fix: change border type

--- lua/plugins/arrow.lua
+++ lua/plugins/arrow.lua
@@ -2,6 +2,7 @@ return {
   'otavioschwanck/arrow.nvim',
   opts = {
     show_icons = true,
+    window = { border = 'single' },
     leader_key = ';', -- Recommended to be a single key
     buffer_leader_key = 'gl', -- Per Buffer Mappings
     index_keys = 'hjkl123456789',
