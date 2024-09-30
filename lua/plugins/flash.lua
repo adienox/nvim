f78d48c6f5790502e87f6a3df1b0f7b8c9fc01ca
Adienox <adwaitadk@pm.me>
2024-10-01 00:24:46 +0545

    feat: update flash keymap

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -5,7 +5,7 @@ return {
   opts = {},
   keys = {
     {
-      '<leader>ss',
+      's',
       mode = { 'n', 'x', 'o' },
       function()
         require('flash').jump()
@@ -13,7 +13,7 @@ return {
       desc = 'Flash',
     },
     {
-      '<leader>sS',
+      'S',
       mode = { 'n', 'x', 'o' },
       function()
         require('flash').treesitter()
