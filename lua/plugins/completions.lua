234b59c92030c5309f1de261085f52fd6bf96472
Adienox <adwaitadk@pm.me>
2024-10-01 19:11:37 +0545

    fix!: remove autopairs logic from cmp

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -43,8 +43,6 @@ return { -- Autocompletion
     local lspkind = require 'lspkind'
     local luasnip = require 'luasnip'
     luasnip.config.setup {}
-    local cmp_autopairs = require 'nvim-autopairs.completion.cmp'
-    cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
 
     cmp.setup {
       formatting = {
